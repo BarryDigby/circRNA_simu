4d3c335a9e5501f08c4e794a65bc81a74b93bd36
Barry digby <b.digby237@gmail.com>
2022-03-01 10:46:19 +0000

    nf command

--- gen_pos.sh
+++ gen_pos.sh
@@ -41,27 +41,25 @@ rm ucsc/hg19.fa
 
 while read -r line; do rm -f ucsc/${line}.fa; done < non_canon_chrs.txt
 
-cat ucsc/*.fa > ucsc/hg19.fa
-
-mv hg19* ucsc/
+cat ucsc/*.fa > hg19.fa
 
 ## 4. Generate Sim reads 
 
 mkdir -p reads
 mkdir -p logs
 
-perl ./CIRIsimulator.pl -1 reads/glio_1.fastq -2 reads/glio_2.fastq -O logs/glio_circRNA.list -G ucsc/hg19.gtf -DB truth_sets/glioblastoma.txt -C 10 -LC 0 -R 1 -LR 1 -L 101 -E 1 -I 350 -D ./ucsc -CHR1 0 -M 50 2>&1 | tee logs/glio.out && \
+perl ./CIRIsimulator.pl -1 reads/glio_1.fastq -2 reads/glio_2.fastq -O logs/glio_circRNA.list -G ./hg19.gtf -DB truth_sets/glioblastoma.txt -C 10 -LC 0 -R 1 -LR 1 -L 101 -E 1 -I 350 -D ./ucsc -CHR1 0 -M 50 2>&1 | tee logs/glio.out && \
 gzip reads/glio_1.fastq reads/glio_2.fastq
 
-perl ./CIRIsimulator.pl -1 reads/oligo_1.fastq -2 reads/oligo_2.fastq -O logs/oligo_circRNA.list -G ucsc/hg19.gtf -DB truth_sets/oligodendroma.txt -C 10 -LC 0 -R 1 -LR 1 -L 101 -E 1 -I 350 -D ./ucsc -CHR1 0 -M 50 2>&1 | tee logs/oligo.out && \
+perl ./CIRIsimulator.pl -1 reads/oligo_1.fastq -2 reads/oligo_2.fastq -O logs/oligo_circRNA.list -G ./hg19.gtf -DB truth_sets/oligodendroma.txt -C 10 -LC 0 -R 1 -LR 1 -L 101 -E 1 -I 350 -D ./ucsc -CHR1 0 -M 50 2>&1 | tee logs/oligo.out && \
 gzip reads/oligo_1.fastq reads/oligo_2.fastq
 
-perl ./CIRIsimulator.pl -1 reads/leuk_1.fastq -2 reads/leuk_2.fastq -O logs/leuk_circRNA.list -G ucsc/hg19.gtf -DB truth_sets/leukemia.txt -C 10 -LC 0 -R 1 -LR 1 -L 101 -E 1 -I 350 -D ./ucsc -CHR1 0 -M 50 2>&1 | tee logs/leuk.out && \
+perl ./CIRIsimulator.pl -1 reads/leuk_1.fastq -2 reads/leuk_2.fastq -O logs/leuk_circRNA.list -G ./hg19.gtf -DB truth_sets/leukemia.txt -C 10 -LC 0 -R 1 -LR 1 -L 101 -E 1 -I 350 -D ./ucsc -CHR1 0 -M 50 2>&1 | tee logs/leuk.out && \
 gzip reads/leuk_1.fastq reads/leuk_2.fastq
 
 ## 5. Run Workflow
 
-nextflow -bg run -r dev nf-core/circrna -profile singularity,lugh --input "reads/*_{1,2}.fastq.gz" --input_type "fastq" --module "circrna_discovery" --tool "circexplorer2, ciriquant, circrna_finder, dcc, find_circ, mapsplice, segemehl" --bsj_reads 0 --tool_filter 0 --outdir "simulated_analysis" --fasta "ucsc/hg19.fa" --gtf "ucsc/hg19.gtf" --circexplorer2_annotation "ucsc/hg19.txt" --max_cpus 16 --max_memory '100.GB'
+nextflow -bg run -r dev nf-core/circrna -profile singularity,lugh --input "reads/*_{1,2}.fastq.gz" --input_type "fastq" --module "circrna_discovery" --tool "circexplorer2, ciriquant, circrna_finder, dcc, find_circ, mapsplice, segemehl" --bsj_reads 0 --tool_filter 0 --outdir "simulated_analysis" --fasta "hg19.fa" --gtf "hg19.gtf" --circexplorer2_annotation "hg19.txt" --max_cpus 16 --max_memory '100.GB'
 
 ## note: you will have to extract each cancer type from the results and check against the truth sets (i.e dont count a circRNA exclusive to glioblastoma as a missed call for the other 2). 
 ## Generate coordinates from the truth sets and use these as members of a set for analysis in R. 
